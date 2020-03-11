3feea8fd4781d4bcf04251ff34c5ced74b653582
PdrZk <60987135+PdrZk@users.noreply.github.com>
2020-03-11 19:22:27 -0300

    Add files via upload

---
+++ Pdr/src/pdr/Pdr.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pdr;
+
+import javax.swing.JFrame;
+
+public class Pdr {
+   
+     
+    public static void main(String[] args) {
+        Issomemo textFieldFrame = new Issomemo();
+        textFieldFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        textFieldFrame.setSize(350,100);
+        textFieldFrame.setVisible(true);
+    }
+    
+}
